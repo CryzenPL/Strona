36027a0334e39e06de8e8feb314a542c63aaf719
CryzenPL <cryzenpl@gmail.com>
2022-05-10 17:57:47 +0200

    Animate

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-alert("Hello world")
+pbutton = document.getElementById("pbutton");
+console.log(pbutton);
+
+pbutton.addEventListener("click", Animation);
+function Animation(e){
+
+pbutton.classList.add('animate__animated', 'animate__bounceOutLeft');
+
+    // console.log(e.target);
+}
