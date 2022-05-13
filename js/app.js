cc6ce8162e24738b58a83c5a611f935487ef5e92
CryzenPL <cryzenpl@gmail.com>
2022-05-13 18:22:59 +0200

    GameV4

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var startPoint = "x";
 var p = [1,2,3,4,5];
+wygrana = [];
 console.log(p[2]);
 remis = 0;
 table.addEventListener("click",clickField);
@@ -18,155 +19,61 @@ function  clickField(e){
         p[x] =  document.getElementById("p"+x).innerHTML;
     }
 
-    // p1 = document.getElementById("p1").innerHTML;
-    // p2 = document.getElementById("p2").innerHTML;
-    // p3 = document.getElementById("p3").innerHTML;
-    // p4 = document.getElementById("p4").innerHTML;
-    // p5 = document.getElementById("p5").innerHTML;
-    // p6 = document.getElementById("p6").innerHTML;
-    // p7 = document.getElementById("p7").innerHTML;
-    // p8 = document.getElementById("p8").innerHTML;
-    // p9 = document.getElementById("p9").innerHTML;
-    wygrana1 = p[1]+p[2]+p[3];
-    wygrana2 = p[4]+p[5]+p[6];
-    wygrana3 = p[7]+p[8]+p[9];
-    wygrana4 = p[1]+p[4]+p[7];
-    wygrana5 = p[2]+p[5]+p[8];
-    wygrana6 = p[3]+p[6]+p[9];
-    wygrana7 = p[1]+p[5]+p[9];
-    wygrana8 = p[3]+p[5]+p[7];
-    console.log(wygrana1);
+    function getElementP(elem,what) {
+        document.getElementById("p"+elem).innerHTML = '<div class="winColor">'+what+'</div>'
+    }
 
+    function getResult(info) {
+        document.getElementById("info").innerHTML = info ;
+    }
 
-    if(wygrana1 == "xxx"){
-        for(x=1 ; x<4 ; x++){
-           document.getElementById("p"+x).innerHTML = '<div class="winColor">x</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Krzyżyków";
+    function wygranaGry(x,kto,par1,par2,par3) {
+        // x - unikalny identyfikator
+        // która konfiguracja wygra
+        // par1,par2,par3 - pola które wygrywają
+        wygrana[x]=p[par1] + p[par2] + p[par3];
+
+        console.log(wygrana[x]);
+            if(wygrana[x] == kto){
+
+                if (kto == "xxx"){
+
+                    getElementP(par1,"x");
+                    getElementP(par2,"x");
+                    getElementP(par3,"x");
+                    getResult("Wygrana Krzyżyków");
+                    table.removeEventListener("click",clickField);
+                    document.getElementById("resetButton").className = "visibleButton";
+                }
+                if (kto == "ooo"){
+
+                    getElementP(par1,"o");
+                    getElementP(par2,"o");
+                    getElementP(par3,"o");
+                    getResult("Wygrana Kółeczków");
+                    table.removeEventListener("click",clickField);
+                    document.getElementById("resetButton").className = "visibleButton";
+                }
+          
+                resetButton.disabled = false;
+            }
     }
-    if(wygrana2 == "xxx"){
-        for(x=4 ; x<7 ; x++){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">x</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Krzyżyków";
+
+    wynik = [];
+    wynik[1] = 'xxx';
+    wynik[2] = 'ooo';
+
+    for(x=1; x<3; x++){
+        wygranaGry(1,wynik[x],1,2,3);
+        wygranaGry(2,wynik[x],4,5,6);
+        wygranaGry(3,wynik[x],7,8,9);
+        wygranaGry(4,wynik[x],1,4,7);
+        wygranaGry(5,wynik[x],2,5,8);
+        wygranaGry(6,wynik[x],3,6,9);
+        wygranaGry(7,wynik[x],1,5,9);
+        wygranaGry(8,wynik[x],3,5,7);
     }
-    if(wygrana3 == "xxx"){
-        for(x=7 ; x<10 ; x++){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">x</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Krzyżyków";
-    }
-    if(wygrana4 == "xxx"){
-        for(x=1 ; x<8 ; x+=3){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">x</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Krzyżyków";
-    }
-    if(wygrana5 == "xxx"){
-        for(x=2 ; x<9 ; x+=3){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">x</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Krzyżyków";
-    }
-    if(wygrana6 == "xxx"){
-        for(x=3 ; x<10 ; x+=3){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">x</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Krzyżyków";
-    }
-    if(wygrana7 == "xxx"){
-        for(x=1 ; x<10 ; x+=4){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">x</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Krzyżyków";
-    }
-    if(wygrana8 == "xxx"){
-        for(x=3 ; x<8 ; x+=2){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">x</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Krzyżyków";
-    }
-    // -----
-    if(wygrana1 == "ooo"){
-        for(x=1 ; x<4 ; x++){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">o</div>'
-         }
-         table.removeEventListener("click",clickField);
-         document.getElementById("resetButton").className = "visibleButton";
-         document.getElementById("info").innerHTML = "Wygrana Kółeczków";
-    }
-    if(wygrana2 == "ooo"){
-        for(x=4 ; x<7 ; x++){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">o</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Kółeczków";
-    }
-    if(wygrana3 == "ooo"){
-        for(x=7 ; x<10 ; x++){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">o</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Kółeczków";
-    }
-    if(wygrana4 == "ooo"){
-        for(x=1 ; x<8 ; x+=3){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">o</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Kółeczków";
-    }
-    if(wygrana5 == "ooo"){
-        for(x=2 ; x<9 ; x+=3){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">o</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Kółeczków";
-    }
-    if(wygrana6 == "ooo"){
-        for(x=3 ; x<10 ; x+=3){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">o</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Kółeczków";
-    }
-    if(wygrana7 == "ooo"){
-        for(x=1 ; x<10 ; x+=4){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">o</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Kółeczków";
-    }
-    if(wygrana8 == "ooo"){
-        for(x=3 ; x<8 ; x+=2){
-            document.getElementById("p"+x).innerHTML = '<div class="winColor">o</div>'
-        }
-        table.removeEventListener("click",clickField);
-        document.getElementById("resetButton").className = "visibleButton";
-        document.getElementById("info").innerHTML = "Wygrana Kółeczków";
-    }
+
     if(remis == 9){
         document.getElementById("resetButton").className = "visibleButton";
     }
