4e221e560d47843b92e22a87a51d02bb64815a2f
CryzenPL <cryzenpl@gmail.com>
2022-05-20 18:27:13 +0200

    GamePV4_MadeAnswers

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var pierwiastki = [];
 var y = 0;
+var odp1 = "";
+//random = 0 ;
 container = document.getElementsByClassName("symbol");
 // console.log(container.length);
 
@@ -22,14 +24,19 @@ console.log(pierwiastki[random]);
 
 for(x = 0 ;  x < container.length ;  x++){
 
-    if(container[x].textContent == pierwiastki[random])
+    if(container[x].textContent == pierwiastki[random]) {
     wylosowany = container[x];
+    }
 }
 console.log(wylosowany)
 wylosowany = wylosowany.parentNode ;
 
 wylosowany.setAttribute("class","checked");
 console.log(wylosowany);
+
+odp1 = pierwiastek[random][1];
+console.log("odp1:"+odp1);
+
 }
 
 easy = document.getElementById("easy");
@@ -40,7 +47,59 @@ easy.addEventListener("click",gameStart);
 medium.addEventListener("click",gameStart);
 hard.addEventListener("click",gameStart);
 
+
 function gameStart(e){
     console.log(e.target.id);
     randomElement();
+    startBtn.classList.add("hide");
+    inputAuto.classList.remove("hide");
+}
+function checkQestion(event){
+    if(event.keyCode == 13){
+    
+        console.log("działa");
+    
+    
+    
+        inputAuto = document.getElementById("inputAuto").value;
+        console.log(inputAuto)
+    
+        random = Math.floor(Math.random() * pierwiastki.length) ;
+        console.log(pierwiastki[random]);
+        
+        odp1 = pierwiastek[random][1];
+        console.log("odp1:"+odp1);
+        
+        for(x = 0 ;  x < container.length ;  x++){
+        
+            if(container[x].textContent == pierwiastki[random])
+            wylosowany = container[x];
+        }
+        console.log(wylosowany)
+        wylosowany = wylosowany.parentNode ;
+        
+        wylosowany.setAttribute("class","checked");
+        console.log(wylosowany);
+        
+        if(odp1 == inputAuto){
+            wylosowany.classList.remove("checked");
+         
+            wylosowany.classList.add("goodAnswer");
+        
+            inputAuto = document.getElementById("inputAuto").value;
+            console.log(inputAuto)
+        }
+        
+        if(odp1 != inputAuto){
+            wylosowany.classList.remove("checked");
+         
+            wylosowany.classList.add("badAnswer");
+        
+            inputAuto = document.getElementById("inputAuto").value;
+            console.log(inputAuto)
+            
+        }
+    
+    
+    }
 }
