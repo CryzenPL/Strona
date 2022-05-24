1016d419b69a0d26a330386d9581bcceda0aeb69
CryzenPL <cryzenpl@gmail.com>
2022-05-24 17:32:38 +0200

    GamePV6_AddedTimer

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ var answer = "";
 var y = 0;
 var k = 0;
 var point = 0;
+var timer = 0;
+var countDownDate = new Date().getTime()+120*1000;
+
 container = document.getElementsByClassName("symbol");
 
 for(x=0; x<container.length; x++){
@@ -51,13 +54,21 @@ function resetGame(){
         elementsTable[x].parentNode.classList.remove("badAnswer");
         elementsTable[x].parentNode.classList.remove("checked");
         elementsTable[x].parentNode.classList.add("grey");
-        elementRandom =  [];
         point = 0;
+        statistic.innerHTML = "<h1>Liczba punktów: "+point+"</h1>";
+        countDownDate =  new Date().getTime()+120*1000;
+        clearInterval(timer);
+        timer = setInterval(countDown,1000);
     }
+    elementRandom =  [];
+    randomElements();
 }
 
 function gameStart(e){
     console.log(e.target.id);
+    countDownDate =  new Date().getTime()+120*1000;
+    clearInterval(timer);
+    timer = setInterval(countDown,1000);
     randomElements();
     document.getElementById("startButton").classList.add("hide");
     document.getElementById("resetButton").classList.remove("hide");
@@ -81,6 +92,28 @@ function checkQestion(event){
         randomElements();
     }
 }
+
+function countDown() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+      
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+      
+    // Time calculations for days, hours, minutes and seconds
+  
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Output the result in an element with id="demo"
+    document.getElementById("time").innerHTML = "<h1>" + minutes + "m " + seconds + "s</h1>";
+      
+    // If the count down is over, write some text 
+    if (distance < 0) {
+      clearInterval(timer);
+      document.getElementById("time").innerHTML = "Koniec Czasu";
+    }
+  }
 
 if(elementRandom.length == elementsTable.length){
   
